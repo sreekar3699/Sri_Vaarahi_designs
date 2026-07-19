a524d337e9b9799c792623bc948720579f0c6f3e
sreekar3699 <paritalasreekar1@gmail.com>
2026-07-19 14:26:13 +0530

    added profile for tests

--- Backend/src/test/java/com/vaarahidesigns/vaarahidesigns/VaarahidesignsApplicationTests.java
+++ Backend/src/test/java/com/vaarahidesigns/vaarahidesigns/VaarahidesignsApplicationTests.java
@@ -4,7 +4,10 @@ import com.vaarahidesigns.vaarahidesigns.main.VaarahidesignsApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest(classes = VaarahidesignsApplication.class)
+@ActiveProfiles("test")
 class VaarahidesignsApplicationTests {
 
 	@Test
